dd7807471751d185a274f3596a2c911ce787f032
MYCOM <MYCOM@DESKTOP-NAMEDPC>
2022-02-18 17:34:13 +0900

    last

--- gaeinProject/src/main/java/boardWeb/util/AdManager.java
+++ gaeinProject/src/main/java/boardWeb/util/AdManager.java
@@ -18,8 +18,8 @@ public class AdManager {
 	Connection conn = null;
 	PreparedStatement psmt = null;
 	ResultSet rs = null;
-	public ArrayList<AdMembers> adMembers = new ArrayList<>();
-	public ArrayList<Integer> midxs = new ArrayList<>();
+	public ArrayList<AdMembers> adMembers = new ArrayList<>();	// 광고를 올린 사람들
+	public ArrayList<Integer> midxs = new ArrayList<>();		// 광고를 올린 사람들의 회원번호를 담는 배열
 	
 	public AdManager() {
 		
@@ -27,11 +27,12 @@ public class AdManager {
 			
 			conn = DBManager.getConnection();
 			
-			sql = "SELECT a.midx, a.point, a.filerealname, a.links, b.nickname, TO_CHAR(adate, 'YYYY-MM-DD') as dates FROM assaad a, assamember b WHERE a.midx = b.midx ORDER BY a.midx";
+			sql = "SELECT a.midx, a.point, a.filerealname, a.links, b.nickname, TO_CHAR(adate, 'YYYY-MM-DD') as dates ";
+			sql +="FROM assaad a, assamember b WHERE a.midx = b.midx ORDER BY a.midx";
 			psmt = conn.prepareStatement(sql);
 			rs = psmt.executeQuery();
 			
-			while(rs.next()) {
+			while(rs.next()) {	// 광고내용을 담는 과정
 				
 				AdMembers adm = new AdMembers();
 				
@@ -46,19 +47,19 @@ public class AdManager {
 				totalpoint += rs.getInt("point");
 			}
 			
-			for(AdMembers a : adMembers) {
-				for(int i = 0; i < a.getPoint(); i++) {
-					midxs.add(a.getMidx());
+			for(AdMembers a : adMembers) {	// 광고를 등록한 사람만큼 반복문
+				for(int i = 0; i < a.getPoint(); i++) {	// 그 사람의 투자한 포인트만큼 반복
+					midxs.add(a.getMidx());	// 그 사람의 회원번호를 배열midxs에추가
 				}
 			}
 			
-			random = (int)((Math.random() * midxs.size()));
+			random = (int)((Math.random() * midxs.size()));	// 배열midxs의 길이만큼 랜덤수를 발생
 			
 			for(AdMembers a : adMembers){
-				if(a.getMidx() == midxs.get(random)){
-					link = a.getLinks();
-					filename = a.getFileRealName();
-					break;
+				if(a.getMidx() == midxs.get(random)){	// 배열midxs의 random번째 인덱스의 값이 등록한 사람의 회원번호가 일치할 때 
+					link = a.getLinks();				// 그 사람것의 링크
+					filename = a.getFileRealName();		// 그 사람것의 파일
+					break;								// 반복문 종료
 				}
 			}
 			
